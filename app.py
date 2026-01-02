57a4f91045b49251d10bace2553246f69d92bcdb
Ewald <alicka.ewald@gmail.com>
2026-01-02 13:21:00 +0100

    Add static images

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import streamlit as st 
 
-st.write("Si jeni")
+st.write("Si jeni te gith")
+
+st.write(True)
+press_me = st.button("Press me")
+print(press_me)
 
 
-st.write("Si jeni")
-st.write("Si jeni")
-st.write("Si jeni")
-st.write("Si jeni")
+# st.write("Si jeni")
+# st.write("Si jeni")
+# st.write("Si jeni")
+# st.write("Si jeni")
 
 
 print('hi')
